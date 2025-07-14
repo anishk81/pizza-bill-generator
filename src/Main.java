7ca50d104e0ea75d59563aa09c8cbceb53676848
Anish K <anish0722@gmail.com>
2025-07-15 00:46:04 +0530

    First Draft of bill made

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Pizza basePrice = new Pizza(false);
-        basePrice.getPizzaPrice();
+        Pizza basePrice = new Pizza(true);
+        basePrice.addExtraToppings();
+        basePrice.addExtraCheese();
+        //basePrice.takeAway();
+        basePrice.getBill();
     }
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -3,6 +3,13 @@ public class Pizza {
 
     private boolean veg;
 
+    private int ExtraCheesePrice = 100;
+    private int ExtraToppings = 140;
+    private int takeAway = 40;
+
+
+
+
     public Pizza(boolean veg) {
         this.veg = veg;
 
@@ -14,7 +21,31 @@ public class Pizza {
         }
     }
 
-    public void getPizzaPrice(){
+    public void addExtraCheese(){
+        System.out.println("Extra Cheese added");
+        this.price += ExtraCheesePrice;
+    }
+
+    public void addExtraToppings(){
+        System.out.println("Extra toppings added of olives, capsicum and onion");
+        this.price += ExtraToppings;
+    }
+
+    public void takeAway(){
+        System.out.println("A carry away backpack added for you convenience");
+        this.price += takeAway;
+    }
+
+    public void getBill(){
         System.out.println(this.price);
     }
 }
+
+
+/*
+ Veg Pizza: 300
+ Non Veg Pizza: 400
+ Extra Cheese: 100
+ Extra Toppings: 140
+ Takeaway: 40
+ */
